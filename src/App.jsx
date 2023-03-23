8e32e3a6d6e2e3c17e1751120ad56565eb1e5894
[Avneesh Jha] <[avneeshjha2015@gmail.com]>
2023-03-23 23:45:31 +0530

    initialised the project and set the basic layout

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import s from "./style.module.css";
+export function App() {
+  return (
+    <div className={s.main_container}>
+      <div className={s.header}>
+        <div className="row">
+          <div className="col-4">
+            <div>LOGO</div>
+            <div>Subtitle</div>
+          </div>
+          <div className="col-md-12 col-lg-4">
+            <input style={{ width: "100%" }} type="text" />
+          </div>
+        </div>
+      </div>
+      <div className={s.tv_show_detail}> tv_show_detail</div>
+      <div className={s.recommended_tv_shows}>recommended_tv_shows</div>
+    </div>
+  );
+}
