82479c0babe99b56a72d8da42d337e9aa610e802
[Avneesh Jha] <[avneeshjha2015@gmail.com]>
2023-03-31 01:09:26 +0530

    feat: tv show details overview title bg and rating

---
+++ src/components/api/fake_data.js
@@ -0,0 +1,18 @@
+export const FAKE_POPULARS = [
+  {
+    backdrop_path: "/lY2DhbA7Hy44fAKddr06UrXWWaQ.jpg",
+    first_air_date: "2023-01-15",
+    genre_ids: [18],
+    id: 100088,
+    name: "The Last of Us",
+    origin_country: ["US"],
+    original_language: "en",
+    original_name: "The Last of Us",
+    overview:
+      "Twenty years after modern civilization has been destroyed, Joel, a hardened survivor, is hired to smuggle Ellie, a 14-year-old girl, out of an oppressive quarantine zone. What starts as a small job soon becomes a brutal, heartbreaking journey, as they both must traverse the United States and depend on each other for survival.",
+    popularity: 2479.952,
+    poster_path: "/uKvVjHNqB5VmOrdxqAt2F7J78ED.jpg",
+    vote_average: 8.8,
+    vote_count: 2942,
+  },
+];
